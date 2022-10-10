566d985b73a6f60c6606cd1272a0d06cb364476a
Dharmik Patel <dharmikp.mobio@gmail.com>
2022-10-10 11:11:17 +0530

    done with json

--- src/Comon.js
+++ src/Comon.js
@@ -2,13 +2,15 @@ import React, { useState } from "react"
 // import parse from "html-react-parser"
 import axios from "axios"
 import { Helmet } from "react-helmet"
+import JSONPretty from 'react-json-pretty';
 // import EmbedContainer from "react-oembed-container"
-
+import 'react-json-pretty/themes/monikai.css';
 const API_KEY = "ce85ecff19fbd7dba1cf97"
 
 export default function OEmbed(props) {
     const [url, setUrl] = useState("")
     const [html, setHtml] = useState({ __html: "<div />" })
+    const [title, setTitle] = useState()
 
     // useEffect(() => {
     //     axios.get(`https://iframe.ly/api/oembed?url=${url}/&api_key=${API_KEY}&iframe=1&omit_script=1`).then((res) => {
@@ -16,34 +18,35 @@ export default function OEmbed(props) {
     //         setHtml({ __html: res.data.html })
     //     })
     // }, [])
-    const submitUrl = () => {
-        axios.get(`https://iframe.ly/api/oembed?url=${url}/&api_key=${API_KEY}&iframe=1&omit_script=1`).then((res) => {
-
-            console.log("res", res)
-            setHtml({ __html: res.data.html })
-
-        })
-        
+    const submitUrl = async () => {
+        const response = await axios.get(`https://iframe.ly/api/oembed?url=${url}/&api_key=${API_KEY}&iframe=1&omit_script=1`)
+        console.log("RES", response)
+        setHtml({ __html: response.data.html })
+        setTitle(response.data)
     }
     if (html) {
         console.log(html, "::::--->>>html")
         return (
             <>
-                <center style={{margin:"50px"}}>
-                <div>
-                    <label style={{marginRight:"10px"}}>URL:</label>
-                    <input type="text" style={{width:"300px"}} name="url" onChange={(e) => setUrl(e.target.value)} />
-                    <button onClick={submitUrl} style={{marginLeft:"10px"}}>Submit</button>
-                </div>
+                <center style={{ margin: "50px" }}>
+                    <div>
+                        <label style={{ marginRight: "10px" }}>URL:</label>
+                        <input type="text" style={{ width: "300px" }} name="url" onChange={(e) => setUrl(e.target.value)} />
+                        <button onClick={submitUrl} style={{ marginLeft: "10px" }}>Submit</button>
+                    </div>
                 </center>
 
+                <div className="data">
+                <JSONPretty data={title} />
+                </div>
 
-                <div style={{margin:"50px"}}>
+                <div style={{ margin: "50px" }}>
                     <Helmet>
                         <script src="https://cdn.iframe.ly/embed.js" async></script>
                     </Helmet>
                     <div dangerouslySetInnerHTML={html} />
                 </div>
+              
             </>
         )
     }
